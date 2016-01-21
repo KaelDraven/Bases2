a39beb78f2f3f81fad21592e9aaaa4b6ab4ecad2
KaelDraven <fallen_versailles@hotmail.com>
2016-01-20 21:32:05 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -1,1 +1,21 @@
+--crearemos una tabla muy simple para probar el poder de ps/sql
+create table dance(id_dance integer, nombre varchar2(40), edad integer);
 
+--prendemos el servidor en modo de desarrollador 
+set serveroutput on;
+
+--vamos a empezar con un bloque pl sql
+
+declare
+--aqui se declaran las variables a usar
+edad integer;
+years integer;
+
+begin
+--a qui va la logica de tu programita
+years:=24;
+edad:=years*365;
+
+dbms_output.put_line('tu edad en dias es: '||edad);
+end;
+/
