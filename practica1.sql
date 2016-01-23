d79d51f552a7700701d4d35094f81f7e96fe4912
KaelDraven <fallen_versailles@hotmail.com>
2016-01-23 10:46:43 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -19,3 +19,26 @@ edad:=years*365;
 dbms_output.put_line('tu edad en dias es: '||edad);
 end;
 /
+--crear un bloque en sql que genere 200000 registro en secuencia acscendente en la tabla dance
+begin
+for i in 1..200000 loop
+insert into dance values (I,'alex',24);
+end loop;
+end;
+/
+select * from dance;
+--Sistema de peliculas la de salas sera tu tarea 
+
+create table pelicula (id_pelicula integer, 
+                        titulo varchar2(120),
+                        sinopsis varchar2(500),
+                        clasificacion varchar2(5),
+                        constraint pk_id_pelicula primary key(id_pelicula) 
+                        );
+describe pelicula;
+
+create table horarios(id_horario integer,
+                      id_pelicula integer,
+                      horario varchar2(8),
+                      constraint pk_id_horario primary key(id_horario),
+                      constraint fk1_id_pelicula foreign key(id_pelicula) references pelicula (id_pelicula));
